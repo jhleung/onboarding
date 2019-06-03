3039ffc9fb61bf914ab906d3426149d2efbc36bc
= <jeffrey.leung@live.com>
2019-06-03 16:00:12 -0500

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+To run this application, 
+	1. run ```git clone https://github.com/jhleung/onboarding``` 
+
+	2. navigate to this project's root directory and run ```node src/js/main.js```
--- src/js/main.js
+++ src/js/main.js
@@ -5,20 +5,19 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 	sendHtmlContent(res, filePath);
 }).listen(9000).on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\n');
+	socket.end('HTTP/1.1 400 Bad Request\n');
 })
 
-function sendHtmlContent(response, fileName){
-	var fs = require('fs');
-  fs.readFile(fileName, function(err, html){
-    if(err){
-      response.writeHead(404);
-      response.write("Not Found!");
-    }
-    else{
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(html);
-    }
+function sendHtmlContent(response, fileName) {
+ 	var fs = require('fs');
+	fs.readFile(fileName, function(err, html) {
+		if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}	else{
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(html);
+		}
 		response.end();
-  });
+	});
 }
