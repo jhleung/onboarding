2f7dca69fae83d31ae9dc0ef988f987c047ceb53
= <jeffrey.leung@live.com>
2019-06-06 11:04:45 -0500

    remove divs when pulling timeline

--- src/js/pullTimeline.js
+++ src/js/pullTimeline.js
@@ -1,31 +1,41 @@
-const formatTimeline = (timeline) => {
-	tweets = JSON.parse(timeline).forEach((tweet, i) => {
-		const timeline = document.createElement("div");
+const formatTimeline = (rawTimeline) => {
+	const timelineDiv = document.getElementById("timeline");
+	var innerTimelineDiv = document.getElementById("timeline-inner");
+	
+	timelineDiv.removeChild(innerTimelineDiv);
+	innerTimelineDiv = document.createElement("div");
+	innerTimelineDiv.id = "timeline-inner";
+
+	tweets = JSON.parse(rawTimeline).forEach((tweet, i) => {
+		const tweetDiv = document.createElement("div");
 
 		if (i % 2 == 0) 
-			timeline.style.backgroundColor="lightsteelblue";
+			tweetDiv.style.backgroundColor="lightsteelblue";
 
 		const profileImg = document.createElement("img");
 		profileImg.src = tweet.profileImageUrl;
-		timeline.appendChild(profileImg);
+		tweetDiv.appendChild(profileImg);
 
 		const timestampSpan = document.createElement("span");
 		const timestamp = new Date(tweet.createdAt);
 		timestampSpan.innerText = timestamp.getFullYear() + "-" + timestamp.getMonth() + "-" + timestamp.getDay() + " " + timestamp.getHours() + ":" + timestamp.getMinutes() + ":" + timestamp.getSeconds();
-		timeline.appendChild(timestampSpan);
+		tweetDiv.appendChild(timestampSpan);
 
-		const message = document.createElement("a");
-		message.target = "_blank";
-		message.rel = "noopener noreferrer";
-		message.href = "https://twitter.com/" + tweet.handle + "/status/" + tweet.id;
-		message.innerHTML = tweet.message;
-		timeline.appendChild(message);
+		const messageLink = document.createElement("a");
+		messageLink.target = "_blank";
+		messageLink.rel = "noopener noreferrer";
+		messageLink.href = "https://twitter.com/" + tweet.handle + "/status/" + tweet.id;
+		messageLink.innerHTML = tweet.message;
+		tweetDiv.appendChild(messageLink);
 
-		document.getElementById("timeline").appendChild(timeline);
-	})
+		innerTimelineDiv.appendChild(tweetDiv);
+	});
+
+	timelineDiv.appendChild(innerTimelineDiv);
 }
 
 const pullTimeline = () => { 
+	var innerTimelineDiv = document.getElementById("timeline-inner");
 	const endpoint = "http://localhost:8080/api/1.0/twitter/timeline"
 	const xhttp = new XMLHttpRequest();
 	xhttp.onload = () => {
@@ -33,12 +43,12 @@ const pullTimeline = () => {
 			formatTimeline(xhttp.responseText);
 		} else if (xhttp.status == 500) {
 			console.log(xhttp.responseText);
-			timelineDiv.innerHTML.innerHTML = "Pull timeline failed.";
+			innerTimelineDiv.innerHTML = "Pull timeline failed.";
 		}
 	};
 	xhttp.onerror = () => {
 	  	console.log("An error has occurred during attempt to make a request to ", endpoint);
-		timelineDiv.innerHTML = "An error has occurred. Please contact system administrator.";
+		innerTimelineDiv.innerHTML = "An error has occurred. Please contact system administrator.";
 	};
 
 	xhttp.open("GET", endpoint, true);
