84e840ee19bc70c06dac02fc8c4d4d3994a18cba
= <jeffrey.leung@live.com>
2019-06-11 16:09:31 -0500

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './src/js/pulltimeline.js',
+  	entry: './src/js/pulltimeline.js',
 	output: {
 		path: path.join(__dirname, '/src'),
-    filename: 'bundle.js'
+    	filename: 'bundle.js'
 	},
 	devServer: {
 		inline: true,
@@ -15,8 +16,19 @@ module.exports = {
 	},
 	module: {
 		rules: [
-		  	{exclude: path.join(__dirname, '/src/js/main.js')}
+		  	{
+		  		exclude: path.join(__dirname, '/src/js/main.js')
+		  	},
+		  	{
+            	test: /\.scss$/,
+            	use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        	}
 		]
-	}
+	},
+	 plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        })
+    ]
 }
 
