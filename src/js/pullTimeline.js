b30635eb45503e4408f7380be4b5eac23719d77c
= <jeffrey.leung@live.com>
2019-06-13 17:34:55 -0500

    convert to react

--- src/js/pullTimeline.js
+++ src/js/pullTimeline.js
@@ -2,128 +2,149 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../main.scss';
 
-const formatTimeline = (rawTimeline) => {
-	const timelineDiv = document.getElementById("timeline");
-	let innerTimelineDiv = document.getElementById("timeline-inner");
-	
-innerTimelineDiv.innerHTML = "";
-	const errorDiv = document.getElementById("error");
-	if (errorDiv != null) 	
-		timelineDiv.removeChild(errorDiv);
+class Timeline extends React.Component {
+    constructor(props) {
+	super(props);
+    }
 
-	timelineDiv.removeChild(innerTimelineDiv);
-	innerTimelineDiv = document.createElement("div");
-	innerTimelineDiv.id = "timeline-inner";
-
-	const obj = JSON.parse(rawTimeline);
+    render() {
+	const obj = JSON.parse(this.props.rawTimeline);
+	let tweets = [];
 	obj.forEach((tweet, i) => {
-		const tweetDiv = document.createElement("div");
-			if (i % 2 == 0) 
-				tweetDiv.className = "tweet-style-one";
-			else 		
-				tweetDiv.className = "tweet-style-two";
+	    let className = i % 2 == 0 ? "tweet-style-one" : "tweet-style-two";
+	    if (i == obj.length - 1)
+		className += "-last-tweet";
 
-			if (i == obj.length - 1)
-				tweetDiv.className += "-last-tweet"
-
-			const profileDiv = document.createElement("div");
-			profileDiv.className = "profile";
-
-			const profileImg = document.createElement("img");
-			profileImg.className = "profile-image";
-			profileImg.src = tweet.user.profileImageUrl;
-			profileDiv.appendChild(profileImg);
-
-			const nameDiv = document.createElement("div");
-			nameDiv.className = "name";
-			nameDiv.innerHTML = tweet.user.name;
-			profileDiv.appendChild(nameDiv);
-
-			const handleDiv = document.createElement("div");
-			handleDiv.className = "handle";
-			handleDiv.innerHTML = tweet.user.handle;
-			profileDiv.appendChild(handleDiv);
-
-			tweetDiv.appendChild(profileDiv);
-
-			const messageDiv = document.createElement("div");
-			messageDiv.className = "message";
-
-			const timestampSpan = document.createElement("div");
-			const timestamp = new Date(tweet.createdAt);
-			timestampSpan.className = "timestamp";
-
-			const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-			timestampSpan.innerText = months[timestamp.getMonth()] + " " + timestamp.getDay();
-			messageDiv.appendChild(timestampSpan);
-
-			const messageLink = document.createElement("a");
-			messageLink.className = "message-link";
-			messageLink.target = "_blank";
-			messageLink.rel = "noopener noreferrer";
-			messageLink.href = "https://twitter.com/${tweet.user.handle}/status/${tweet.id}";
-			messageLink.innerHTML = tweet.message;
-			messageDiv.appendChild(messageLink);
-
-			tweetDiv.appendChild(messageDiv);
-
-			innerTimelineDiv.appendChild(tweetDiv);
+	    tweets.push(<Tweet key={i} tweet={tweet} className={className}/>);
 	});
 
-	timelineDiv.appendChild(innerTimelineDiv);
+	return(
+	    <div id="timeline-inner">{tweets}</div>
+	);
+    }
 }
 
-const pullTimeline = () => { 
-	const timelineDiv = document.getElementById("timeline");
-	let innerTimelineDiv = document.getElementById("timeline-inner");
+class Tweet extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	return(
+	    <div className={this.props.className}>
+		<Profile profileImageUrl={this.props.tweet.user.profileImageUrl} userName={this.props.tweet.user.name} handle={this.props.tweet.user.handle}/>	
+		<Message createdAt={this.props.tweet.createdAt} message={this.props.tweet.message} handle={this.props.tweet.user.handle} id={this.props.tweet.id}/>
+	    </div>
+	);
+    }
+}
+
+class Profile extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	return(
+	    <div className="profile">
+		<img className="profile-image" src={this.props.profileImageUrl}/>
+		<div className="name">{this.props.userName}</div>
+		<div className="handle">{this.props.handle}</div>
+	    </div>
+	);
+    }
+}
+
+class Message extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	const date = new Date(this.props.createdAt);
+	let timestamp = months[date.getMonth()] + " " + date.getDay();
+
+	let tweetUrl = `https://twitter.com/${this.props.handle}/status/${this.props.id}`
+	return(
+	    <div className="message">
+		<div className="timestamp">{timestamp}</div>
+		<a className="message-link" target="_blank" rel="noopener noreferrer" href={tweetUrl}>{this.props.message}</a>
+	    </div>
+	);
+    }
+}
+
+class Error extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	return(<div className="error">{this.props.errorMsg}</div>);
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+	super(props);
+
+	this.state = {
+	    timeline: null,
+	    errorMsg: null,
+	    isError: true,
+	};
+    }
+
+    componentWillMount() {
+	this.pullTimeline();
+    }
+
+    pullTimeline() {
 	const endpoint = "http://localhost:8080/api/1.0/twitter/timeline"
 	const xhttp = new XMLHttpRequest();
 	xhttp.onload = () => {
-		if (xhttp.status == 200) {
-			formatTimeline(xhttp.responseText);
-		} else if (xhttp.status == 500) {
-			console.log(xhttp.responseText);
-			timelineDiv.removeChild(innerTimelineDiv);
-			const errorMsg = document.createElement("div");
-			errorMsg.className = "error";
-			errorMsg.innerHTML = "Pull timeline failed.";
-			innerTimelineDiv = document.createElement("div");
-			innerTimelineDiv.id = "timeline-inner";
-			innerTimelineDiv.appendChild(errorMsg);
-			timelineDiv.appendChild(innerTimelineDiv);
-
-		}
+	    if (xhttp.status == 200) {
+		this.state.timeline = xhttp.responseText;
+		this.state.isError = false;
+	    } else if (xhttp.status == 500) {
+		console.log(xhttp.responseText);
+		this.state.errorMsg = "Pull timeline failed.";
+		this.state.isError = true;
+	    }
+	    this.setState(this.state);
 	};
+
 	xhttp.onerror = () => {
-		console.log("An error has occurred during attempt to make a request to ", endpoint);
-		timelineDiv.removeChild(innerTimelineDiv);
-		const errorMsg = document.createElement("div");
-		errorMsg.className = "error";
-		errorMsg.innerHTML = "An error has occurred. Please contact system administrator.";
-		innerTimelineDiv = document.createElement("div");	
-		innerTimelineDiv.id = "timeline-inner";
-		innerTimelineDiv.appendChild(errorMsg);
-		timelineDiv.appendChild(innerTimelineDiv);
+	    console.log(`An error has occurred during attempt to make a request to  ${endpoint}`);
+	    this.state.errorMsg = "An error has occurred. Please contact system administrator.";
+	    this.state.isError = true;
+	    this.setState(this.state);
 	};
 
 	xhttp.open("GET", endpoint, true);
 	xhttp.send(); 
-};
+    };
 
-window.onload = () => { 
-	pullTimeline();
-	document.getElementById("pullTimeline").onclick = pullTimeline;
+    render() {
+	let timeline = this.state.isError ? <Error errorMsg={this.state.errorMsg}/> : <Timeline rawTimeline={this.state.timeline}/>
+	    return(
+		<div className="app">
+		    <div id="header">
+			<button type="button" id="pullTimeline" onClick={() => this.pullTimeline()}>Pull timeline</button>
+		    </div>
+		    <div id="timeline">{timeline}</div>	
+		</div>
+	    );
+    }
 }
 
 
 class HelloReact extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return(<h1>{"hello react!"}</h1>);
-  }
+    render() {
+	return(<p>{"hello react!"}</p>);
+    }
 }
 
+ReactDOM.render(<App />, document.getElementById("app"));
 ReactDOM.render(<HelloReact />, document.getElementById('hello-react'));
