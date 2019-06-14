e871ad81fe909cd3db9856cdb70e9ee284d227f8
= <jeffrey.leung@live.com>
2019-06-14 16:50:41 -0500

    white space

--- src/js/pullTimeline.js
+++ src/js/pullTimeline.js
@@ -2,114 +2,114 @@ import '../main.scss';
 import './helloReact.js';
 
 const formatTimeline = (rawTimeline) => {
-	const timelineDiv = document.getElementById("timeline");
-	let innerTimelineDiv = document.getElementById("timeline-inner");
-	
-	innerTimelineDiv.innerHTML = "";
-	const errorDiv = document.getElementById("error");
-	if (errorDiv != null) 	
-		timelineDiv.removeChild(errorDiv);
+    const timelineDiv = document.getElementById("timeline");
+    let innerTimelineDiv = document.getElementById("timeline-inner");
 
-	timelineDiv.removeChild(innerTimelineDiv);
-	innerTimelineDiv = document.createElement("div");
-	innerTimelineDiv.id = "timeline-inner";
+    innerTimelineDiv.innerHTML = "";
+    const errorDiv = document.getElementById("error");
+    if (errorDiv != null) 	
+	timelineDiv.removeChild(errorDiv);
 
-	const obj = JSON.parse(rawTimeline);
-	obj.forEach((tweet, i) => {
-		const tweetDiv = document.createElement("div");
-		if (i % 2 == 0) 
-			tweetDiv.className = "tweet-style-one";
-		else 		
-			tweetDiv.className = "tweet-style-two";
+    timelineDiv.removeChild(innerTimelineDiv);
+    innerTimelineDiv = document.createElement("div");
+    innerTimelineDiv.id = "timeline-inner";
 
-		if (i == obj.length - 1)
-			tweetDiv.className += "-last-tweet"
+    const obj = JSON.parse(rawTimeline);
+    obj.forEach((tweet, i) => {
+	const tweetDiv = document.createElement("div");
+	if (i % 2 == 0) 
+	    tweetDiv.className = "tweet-style-one";
+	else 		
+	    tweetDiv.className = "tweet-style-two";
 
-		const profileDiv = document.createElement("div");
-		profileDiv.className = "profile";
+	if (i == obj.length - 1)
+	    tweetDiv.className += "-last-tweet"
 
-		const profileImg = document.createElement("img");
-		profileImg.className = "profile-image";
-		profileImg.src = tweet.user.profileImageUrl;
-		profileDiv.appendChild(profileImg);
+	const profileDiv = document.createElement("div");
+	profileDiv.className = "profile";
 
-		const nameDiv = document.createElement("div");
-		nameDiv.className = "name";
-		nameDiv.innerHTML = tweet.user.name;
-		profileDiv.appendChild(nameDiv);
+	const profileImg = document.createElement("img");
+	profileImg.className = "profile-image";
+	profileImg.src = tweet.user.profileImageUrl;
+	profileDiv.appendChild(profileImg);
 
-		const handleDiv = document.createElement("div");
-		handleDiv.className = "handle";
-		handleDiv.innerHTML = tweet.user.handle;
-		profileDiv.appendChild(handleDiv);
+	const nameDiv = document.createElement("div");
+	nameDiv.className = "name";
+	nameDiv.innerHTML = tweet.user.name;
+	profileDiv.appendChild(nameDiv);
 
-		tweetDiv.appendChild(profileDiv);
+	const handleDiv = document.createElement("div");
+	handleDiv.className = "handle";
+	handleDiv.innerHTML = tweet.user.handle;
+	profileDiv.appendChild(handleDiv);
 
-		const messageDiv = document.createElement("div");
-		messageDiv.className = "message";
+	tweetDiv.appendChild(profileDiv);
 
-		const timestampSpan = document.createElement("div");
-		const timestamp = new Date(tweet.createdAt);
-		timestampSpan.className = "timestamp";
+	const messageDiv = document.createElement("div");
+	messageDiv.className = "message";
 
-		const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-		timestampSpan.innerText = months[timestamp.getMonth()] + " " + timestamp.getDay();
-		messageDiv.appendChild(timestampSpan);
+	const timestampSpan = document.createElement("div");
+	const timestamp = new Date(tweet.createdAt);
+	timestampSpan.className = "timestamp";
 
-		const messageLink = document.createElement("a");
-		messageLink.className = "message-link";
-		messageLink.target = "_blank";
-		messageLink.rel = "noopener noreferrer";
-		messageLink.href = `https://twitter.com/${tweet.user.handle}/status/${tweet.id}`;
-		messageLink.innerHTML = tweet.message;
-		messageDiv.appendChild(messageLink);
+	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	timestampSpan.innerText = months[timestamp.getMonth()] + " " + timestamp.getDay();
+	messageDiv.appendChild(timestampSpan);
 
-		tweetDiv.appendChild(messageDiv);
+	const messageLink = document.createElement("a");
+	messageLink.className = "message-link";
+	messageLink.target = "_blank";
+	messageLink.rel = "noopener noreferrer";
+	messageLink.href = `https://twitter.com/${tweet.user.handle}/status/${tweet.id}`;
+	messageLink.innerHTML = tweet.message;
+	messageDiv.appendChild(messageLink);
 
-		innerTimelineDiv.appendChild(tweetDiv);
-	});
+	tweetDiv.appendChild(messageDiv);
 
-	timelineDiv.appendChild(innerTimelineDiv);
+	innerTimelineDiv.appendChild(tweetDiv);
+    });
+
+    timelineDiv.appendChild(innerTimelineDiv);
 }
 
 const pullTimeline = () => { 
-	const timelineDiv = document.getElementById("timeline");
-	let innerTimelineDiv = document.getElementById("timeline-inner");
-	const endpoint = "http://localhost:8080/api/1.0/twitter/timeline"
-	const xhttp = new XMLHttpRequest();
-	xhttp.onload = () => {
-		if (xhttp.status == 200) {
-			formatTimeline(xhttp.responseText);
-		} else if (xhttp.status == 500) {
-			console.log(xhttp.responseText);
-			timelineDiv.removeChild(innerTimelineDiv);
-			const errorMsg = document.createElement("div");
-			errorMsg.className = "error";
-			errorMsg.innerHTML = "Pull timeline failed.";
-			innerTimelineDiv = document.createElement("div");
-			innerTimelineDiv.id = "timeline-inner";
-			innerTimelineDiv.appendChild(errorMsg);
-			timelineDiv.appendChild(innerTimelineDiv);
+    const timelineDiv = document.getElementById("timeline");
+    let innerTimelineDiv = document.getElementById("timeline-inner");
+    const endpoint = "http://localhost:8080/api/1.0/twitter/timeline"
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = () => {
+	if (xhttp.status == 200) {
+	    formatTimeline(xhttp.responseText);
+	} else if (xhttp.status == 500) {
+	    console.log(xhttp.responseText);
+	    timelineDiv.removeChild(innerTimelineDiv);
+	    const errorMsg = document.createElement("div");
+	    errorMsg.className = "error";
+	    errorMsg.innerHTML = "Pull timeline failed.";
+	    innerTimelineDiv = document.createElement("div");
+	    innerTimelineDiv.id = "timeline-inner";
+	    innerTimelineDiv.appendChild(errorMsg);
+	    timelineDiv.appendChild(innerTimelineDiv);
 
-		}
-	};
-	xhttp.onerror = () => {
-		console.log(`An error has occurred during attempt to make a request to  ${endpoint}`);
-		timelineDiv.removeChild(innerTimelineDiv);
-		const errorMsg = document.createElement("div");
-		errorMsg.className = "error";
-		errorMsg.innerHTML = "An error has occurred. Please contact system administrator.";
-		innerTimelineDiv = document.createElement("div");	
-		innerTimelineDiv.id = "timeline-inner";
-		innerTimelineDiv.appendChild(errorMsg);
-		timelineDiv.appendChild(innerTimelineDiv);
-	};
+	}
+    };
+    xhttp.onerror = () => {
+	console.log(`An error has occurred during attempt to make a request to  ${endpoint}`);
+	timelineDiv.removeChild(innerTimelineDiv);
+	const errorMsg = document.createElement("div");
+	errorMsg.className = "error";
+	errorMsg.innerHTML = "An error has occurred. Please contact system administrator.";
+	innerTimelineDiv = document.createElement("div");	
+	innerTimelineDiv.id = "timeline-inner";
+	innerTimelineDiv.appendChild(errorMsg);
+	timelineDiv.appendChild(innerTimelineDiv);
+    };
 
-	xhttp.open("GET", endpoint, true);
-	xhttp.send(); 
+    xhttp.open("GET", endpoint, true);
+    xhttp.send(); 
 };
 
 window.onload = () => { 
-	pullTimeline();
-	document.getElementById("pullTimeline").onclick = pullTimeline;
+    pullTimeline();
+    document.getElementById("pullTimeline").onclick = pullTimeline;
 }
