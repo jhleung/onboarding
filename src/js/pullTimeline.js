1dd5d418bd2af1146e4f8e73cc2629fbf1ef4db5
= <jeffrey.leung@live.com>
2019-06-14 17:07:41 -0500

    double to single quotes

--- src/js/pullTimeline.js
+++ src/js/pullTimeline.js
@@ -2,64 +2,64 @@ import '../main.scss';
 import './helloReact.js';
 
 const formatTimeline = (rawTimeline) => {
-    const timelineDiv = document.getElementById("timeline");
-    let innerTimelineDiv = document.getElementById("timeline-inner");
+    const timelineDiv = document.getElementById('timeline');
+    let innerTimelineDiv = document.getElementById('timeline-inner');
 
-    innerTimelineDiv.innerHTML = "";
-    const errorDiv = document.getElementById("error");
+    innerTimelineDiv.innerHTML = '';
+    const errorDiv = document.getElementById('error');
     if (errorDiv != null) 	
 	timelineDiv.removeChild(errorDiv);
 
     timelineDiv.removeChild(innerTimelineDiv);
-    innerTimelineDiv = document.createElement("div");
-    innerTimelineDiv.id = "timeline-inner";
+    innerTimelineDiv = document.createElement('div');
+    innerTimelineDiv.id = 'timeline-inner';
 
     const obj = JSON.parse(rawTimeline);
     obj.forEach((tweet, i) => {
-	const tweetDiv = document.createElement("div");
+	const tweetDiv = document.createElement('div');
 	if (i % 2 == 0) 
-	    tweetDiv.className = "tweet-style-one";
+	    tweetDiv.className = 'tweet-style-one';
 	else 		
-	    tweetDiv.className = "tweet-style-two";
+	    tweetDiv.className = 'tweet-style-two';
 
 	if (i == obj.length - 1)
-	    tweetDiv.className += "-last-tweet"
+	    tweetDiv.className += '-last-tweet'
 
-	const profileDiv = document.createElement("div");
-	profileDiv.className = "profile";
+	const profileDiv = document.createElement('div');
+	profileDiv.className = 'profile';
 
-	const profileImg = document.createElement("img");
-	profileImg.className = "profile-image";
+	const profileImg = document.createElement('img');
+	profileImg.className = 'profile-image';
 	profileImg.src = tweet.user.profileImageUrl;
 	profileDiv.appendChild(profileImg);
 
-	const nameDiv = document.createElement("div");
-	nameDiv.className = "name";
+	const nameDiv = document.createElement('div');
+	nameDiv.className = 'name';
 	nameDiv.innerHTML = tweet.user.name;
 	profileDiv.appendChild(nameDiv);
 
-	const handleDiv = document.createElement("div");
-	handleDiv.className = "handle";
+	const handleDiv = document.createElement('div');
+	handleDiv.className = 'handle';
 	handleDiv.innerHTML = tweet.user.handle;
 	profileDiv.appendChild(handleDiv);
 
 	tweetDiv.appendChild(profileDiv);
 
-	const messageDiv = document.createElement("div");
-	messageDiv.className = "message";
+	const messageDiv = document.createElement('div');
+	messageDiv.className = 'message';
 
-	const timestampSpan = document.createElement("div");
+	const timestampSpan = document.createElement('div');
 	const timestamp = new Date(tweet.createdAt);
-	timestampSpan.className = "timestamp";
+	timestampSpan.className = 'timestamp';
 
 	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	timestampSpan.innerText = months[timestamp.getMonth()] + " " + timestamp.getDay();
+	timestampSpan.innerText = months[timestamp.getMonth()] + ' ' + timestamp.getDay();
 	messageDiv.appendChild(timestampSpan);
 
-	const messageLink = document.createElement("a");
-	messageLink.className = "message-link";
-	messageLink.target = "_blank";
-	messageLink.rel = "noopener noreferrer";
+	const messageLink = document.createElement('a');
+	messageLink.className = 'message-link';
+	messageLink.target = '_blank';
+	messageLink.rel = 'noopener noreferrer';
 	messageLink.href = `https://twitter.com/${tweet.user.handle}/status/${tweet.id}`;
 	messageLink.innerHTML = tweet.message;
 	messageDiv.appendChild(messageLink);
@@ -73,9 +73,9 @@ const formatTimeline = (rawTimeline) => {
 }
 
 const pullTimeline = () => { 
-    const timelineDiv = document.getElementById("timeline");
-    let innerTimelineDiv = document.getElementById("timeline-inner");
-    const endpoint = "http://localhost:8080/api/1.0/twitter/timeline"
+    const timelineDiv = document.getElementById('timeline');
+    let innerTimelineDiv = document.getElementById('timeline-inner');
+    const endpoint = 'http://localhost:8080/api/1.0/twitter/timeline'
     const xhttp = new XMLHttpRequest();
     xhttp.onload = () => {
 	if (xhttp.status == 200) {
@@ -83,11 +83,11 @@ const pullTimeline = () => {
 	} else if (xhttp.status == 500) {
 	    console.log(xhttp.responseText);
 	    timelineDiv.removeChild(innerTimelineDiv);
-	    const errorMsg = document.createElement("div");
-	    errorMsg.className = "error";
-	    errorMsg.innerHTML = "Pull timeline failed.";
-	    innerTimelineDiv = document.createElement("div");
-	    innerTimelineDiv.id = "timeline-inner";
+	    const errorMsg = document.createElement('div');
+	    errorMsg.className = 'error';
+	    errorMsg.innerHTML = 'Pull timeline failed.';
+	    innerTimelineDiv = document.createElement('div');
+	    innerTimelineDiv.id = 'timeline-inner';
 	    innerTimelineDiv.appendChild(errorMsg);
 	    timelineDiv.appendChild(innerTimelineDiv);
 
@@ -96,20 +96,20 @@ const pullTimeline = () => {
     xhttp.onerror = () => {
 	console.log(`An error has occurred during attempt to make a request to  ${endpoint}`);
 	timelineDiv.removeChild(innerTimelineDiv);
-	const errorMsg = document.createElement("div");
-	errorMsg.className = "error";
-	errorMsg.innerHTML = "An error has occurred. Please contact system administrator.";
-	innerTimelineDiv = document.createElement("div");	
-	innerTimelineDiv.id = "timeline-inner";
+	const errorMsg = document.createElement('div');
+	errorMsg.className = 'error';
+	errorMsg.innerHTML = 'An error has occurred. Please contact system administrator.';
+	innerTimelineDiv = document.createElement('div');	
+	innerTimelineDiv.id = 'timeline-inner';
 	innerTimelineDiv.appendChild(errorMsg);
 	timelineDiv.appendChild(innerTimelineDiv);
     };
 
-    xhttp.open("GET", endpoint, true);
+    xhttp.open('GET', endpoint, true);
     xhttp.send(); 
 };
 
 window.onload = () => { 
     pullTimeline();
-    document.getElementById("pullTimeline").onclick = pullTimeline;
+    document.getElementById('pullTimeline').onclick = pullTimeline;
 }
