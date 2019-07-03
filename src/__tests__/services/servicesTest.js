70fb91c0ac238cbde3a3cd0867467bd9f9b86688
= <jeffrey.leung@live.com>
2019-07-03 12:30:10 -0500

    test post tweets service

--- src/__tests__/services/servicesTest.js
+++ src/__tests__/services/servicesTest.js
@@ -1,4 +1,4 @@
-import {fetchTimeline} from '../../js/services/service.js';   
+import {fetchTimeline, publishTweet} from '../../js/services/service.js';   
 
 const createMockPromiseResolved = (status)=> {
 	const response = jest.fn();
@@ -79,6 +79,48 @@ describe('Pull user timeline', () => {
 		const mockPromise = createMockPromiseRejected();
 		window.fetch = jest.fn(() => mockPromise);
 		await fetchTimeline(endpoint).catch((res) => expect(res).toEqual(`An error has occurred. Please contact system administrator.`));
+	});
+
+});
+
+
+describe('Push tweet', () => {
+	const oldFetch = window.fetch;
+	const oldHeaders = window.Headers;
+	const endpoint = 'http://localhost:8080/api/1.0/twitter/tweet';
+
+	afterEach(() => {
+		window.fetch = oldFetch;
+		window.Headers = oldHeaders;
+	});
+
+	it('should make call to correct endpoint', async () => {
+		const mockPromise = createMockPromiseResolved(200);
+		window.fetch = jest.fn(() => mockPromise);
+		window.Headers = jest.fn();
+		await publishTweet('test');
+		expect(fetch).toHaveBeenCalledWith(endpoint, {"body": "message=test", "headers": {}, "method": "POST"});
+	});
+
+	it('should get success response from api call', async () => {
+		const mockPromise = createMockPromiseResolved(200);
+		window.fetch = jest.fn(() => mockPromise);
+		window.Headers = jest.fn();
+		await publishTweet('test').then((res) => expect(res).toEqual('success'));
+	});
+
+	it('should get error response from api call', async () => {
+		const mockPromise = createMockPromiseResolved(500);
+		window.fetch = jest.fn(() => mockPromise);
+		window.Headers = jest.fn();
+		await publishTweet('test').catch((res) => expect(res).toEqual('Post tweet failed.'));
+	});
+
+	it('should get network error response', async () => {
+		const mockPromise = createMockPromiseRejected();
+		window.fetch = jest.fn(() => mockPromise);
+		window.Headers = jest.fn();
+		await publishTweet('test').catch((res) => expect(res).toEqual(`An error has occurred. Please contact system administrator.`));
 	});
 
 });
